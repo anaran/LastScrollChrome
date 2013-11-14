7fc3a19886da83bb2222c89f6b531f1ce616a851
Adrian Aichner <adrian.aichner@gmail.com>
2013-11-14 22:34:53 +0100

    Fix corruption of scrolling.
    Replace document.body.clientHeight with window.innerHeight.
    Replace document.body.clientWidth with window.innerWidth.

--- LastScroll/last_scroll.js
+++ LastScroll/last_scroll.js
@@ -3,7 +3,6 @@
     var lastStyle = window.getComputedStyle(document.body);
     var div = document.createElement('div');
     div.style.position = "absolute";
-    //    div.style.opacity = 0.7;
     div.style.fontSize = "xx-large";
     div.title = chrome.i18n.getMessage('extension_name') + "\n" + chrome.i18n.getMessage('extension_description');
     div.style.padding = "3px";
@@ -16,16 +15,16 @@
         if (div.parentElement) {
             document.body.removeChild(div);
         }
-        div.style.left = document.body.scrollLeft + document.body.clientWidth / 2 + "px";
+        div.style.left = document.body.scrollLeft + window.innerWidth / 2 + "px";
         var scrollingUp = lastScrollTop > document.body.scrollTop;
-        if (Math.abs(lastScrollTop - document.body.scrollTop) > document.body.clientHeight * 0.5) {
+        if (Math.abs(lastScrollTop - document.body.scrollTop) > window.innerHeight * 0.5) {
             // TODO Please note we are scrolling by page here.
             if (scrollingUp) {
                 div.innerHTML = "&DoubleUpArrow;";
                 div.style.top = (lastScrollTop) + "px";
             } else {
                 div.innerHTML = "&DoubleDownArrow;";
-                div.style.top = (lastScrollTop + document.body.clientHeight) + "px";
+                div.style.top = (lastScrollTop + window.innerHeight) + "px";
             }
         } else {
             // TODO Please note we are scrolling by line here.
@@ -34,15 +33,14 @@
                 div.style.top = (document.body.scrollTop) + "px";
             } else {
                 div.innerHTML = "&Darr;";
-                div.style.top = (lastScrollTop + document.body.clientHeight) + "px";
+                div.style.top = (lastScrollTop + window.innerHeight) + "px";
             }
-
         }
         document.body.appendChild(div);
-        //         window.scrollTo(div.getBoundingClientRect().left, div.getBoundingClientRect().top);
-        //        console.log(JSON.stringify(document.body, [
-        //            'clientHeight', 'clientLeft', 'clientTop', 'clientWidth',
-        //            'scrollHeight', 'scrollLeft', 'scrollTop', 'scrollWidth'], 2));
+        //window.scrollTo(div.getBoundingClientRect().left, div.getBoundingClientRect().top);
+        //console.log(JSON.stringify(document.body, [
+        //    'clientHeight', 'clientLeft', 'clientTop', 'clientWidth',
+        //    'scrollHeight', 'scrollLeft', 'scrollTop', 'scrollWidth'], 2));
         lastScrollTop = document.body.scrollTop;
     }, true);
 })();
