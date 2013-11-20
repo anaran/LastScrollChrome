6a895ee992d2f5653e95ed4378b198ab614e5b6d
Adrian Aichner <adrian.aichner@gmail.com>
2013-11-20 18:11:17 +0100

    Revert to use document.body.scrollTop.
    document.documentElement.scrollTop is always 0 in Google Chrome 31, 32.
    This is version 9.
    Add unified console.time and log reporting among my extensions.
    Don't load into all_frames (would load in svg frame of ecmascript spec.
    Move init into document.addEventListener('readystatechange', function(event) {

--- LastScroll/last_scroll.js
+++ LastScroll/last_scroll.js
@@ -1,59 +1,71 @@
 (function() {
-    var lastScrollTop = document.documentElement.scrollTop;
-    //var lastStyle = window.getComputedStyle(document.body);
-    var div = document.createElement('div');
-    div.style.position = "absolute";
-    div.style.fontSize = "xx-large";
-    div.title = chrome.i18n.getMessage('extension_name') + "\n" + chrome.i18n.getMessage('extension_description');
-    div.style.padding = "3px";
-    div.style.borderRadius = "3px";
-    //TODO I don't really want to do this, but Last Scoll is sometimes not visible in the wild,
-    //div.style.zIndex = 100;
-    // OK, here might be a general solution:
-    function whoIsInFrontOf(div) {
-        return document.elementFromPoint((div.getBoundingClientRect().right + div.getBoundingClientRect().left) / 2, (div.getBoundingClientRect().top + div.getBoundingClientRect().bottom) / 2);
-    }
-    div.className = "lastScroll";
-    if (div.parentElement) {
-        document.body.removeChild(div);
-    }
-    window.addEventListener('scroll', function(event) {
+    var loading = "loading started at " + new Error().stack.split(/\s+/)[2] + "\n(" + (chrome.app.getDetails() && chrome.app.getDetails().name || "no chrome.app.getDetails()") + ") takes";
+    console.time(loading);
+    //TODO Place following code where timed section should end.
+    //console.timeEnd(loading);
+    //console.log("Reload it with Ctrl+R or as follows:\nlocation.reload(true)");
+    //console.log("injection into " + document.URL + " in\n" + JSON.stringify(navigator.userAgent) + "\nends at\n" + JSON.stringify(Date()));
+    document.addEventListener('readystatechange', function(event) {
+        if (event.target.readyState !== "complete") {
+            return;
+        }
+        var lastScrollTop = document.body.scrollTop;
+        //var lastStyle = window.getComputedStyle(document.body);
+        var div = document.createElement('div');
+        div.style.position = "absolute";
+        div.style.fontSize = "xx-large";
+        div.title = chrome.i18n.getMessage('extension_name') + "\n" + chrome.i18n.getMessage('extension_description');
+        div.style.padding = "3px";
+        div.style.borderRadius = "3px";
+        // OK, here might be a general solution:
+        function whoIsInFrontOf(element) {
+            return document.elementFromPoint((element.getBoundingClientRect().right + element.getBoundingClientRect().left) / 2, (element.getBoundingClientRect().top + element.getBoundingClientRect().bottom) / 2);
+        }
+        div.className = "lastScroll";
         if (div.parentElement) {
             document.body.removeChild(div);
         }
-        div.style.left = document.documentElement.scrollLeft + window.innerWidth / 2 + "px";
-        var scrollingUp = lastScrollTop > document.documentElement.scrollTop;
-        if (Math.abs(lastScrollTop - document.documentElement.scrollTop) > window.innerHeight * 0.5) {
-            // TODO Please note we are scrolling by page here.
-            if (scrollingUp) {
-                div.innerHTML = "&DoubleUpArrow;";
-                div.style.top = (lastScrollTop) + "px";
-            } else {
-                div.innerHTML = "&DoubleDownArrow;";
-                div.style.top = (lastScrollTop + window.innerHeight) + "px";
+        window.addEventListener('scroll', function(event) {
+            if (div.parentElement) {
+                document.body.removeChild(div);
             }
-        } else {
-            // TODO Please note we are scrolling by line here.
-            if (scrollingUp) {
-                div.innerHTML = "&Uarr;";
-                div.style.top = (document.documentElement.scrollTop) + "px";
+            div.style.left = document.body.scrollLeft + window.innerWidth / 2 + "px";
+            var scrollingUp = lastScrollTop > document.body.scrollTop;
+            if (Math.abs(lastScrollTop - document.body.scrollTop) > window.innerHeight * 0.5) {
+                // TODO Please note we are scrolling by page here.
+                if (scrollingUp) {
+                    div.innerHTML = "&DoubleUpArrow;";
+                    div.style.top = (lastScrollTop) + "px";
+                } else {
+                    div.innerHTML = "&DoubleDownArrow;";
+                    div.style.top = (lastScrollTop + window.innerHeight) + "px";
+                }
             } else {
-                div.innerHTML = "&Darr;";
-                div.style.top = (lastScrollTop + window.innerHeight) + "px";
+                // TODO Please note we are scrolling by line here.
+                if (scrollingUp) {
+                    div.innerHTML = "&Uarr;";
+                    div.style.top = (document.body.scrollTop) + "px";
+                } else {
+                    div.innerHTML = "&Darr;";
+                    div.style.top = (lastScrollTop + window.innerHeight) + "px";
+                }
             }
-        }
-        document.body.appendChild(div);
-        // I ever get numeric zIndex, so I have to succumb to try getting in front iteratively.
-        // var otherZindex = Number(window.getComputedStyle(who).zIndex);
-        for (var zIndex = 1, who = whoIsInFrontOf(div); who && who !== div && zIndex <= 10000; zIndex *= 10) {
-            console.info('increasing z-index to ', zIndex, ' trying to get in front of ', who);
-            div.style.zIndex = zIndex;
-            who = whoIsInFrontOf(div);
-        }
-        //window.scrollTo(div.getBoundingClientRect().left, div.getBoundingClientRect().top);
-        //console.log(JSON.stringify(document.body, [
-        //    'clientHeight', 'clientLeft', 'clientTop', 'clientWidth',
-        //    'scrollHeight', 'scrollLeft', 'scrollTop', 'scrollWidth'], 2));
-        lastScrollTop = document.documentElement.scrollTop;
-    }, true);
+            document.body.appendChild(div);
+            // I ever get numeric zIndex, so I have to succumb to try getting in front iteratively.
+            // var otherZindex = Number(window.getComputedStyle(who).zIndex);
+            for (var zIndex = 1, who = whoIsInFrontOf(div); who && who !== div && zIndex <= 10000; zIndex *= 10) {
+                console.info('increasing z-index to ', zIndex, ' trying to get in front of ', who);
+                div.style.zIndex = zIndex;
+                who = whoIsInFrontOf(div);
+            }
+            //window.scrollTo(div.getBoundingClientRect().left, div.getBoundingClientRect().top);
+            //console.log(JSON.stringify(document.body, [
+            //    'clientHeight', 'clientLeft', 'clientTop', 'clientWidth',
+            //    'scrollHeight', 'scrollLeft', 'scrollTop', 'scrollWidth'], 2));
+            lastScrollTop = document.body.scrollTop;
+        }, true);
+    }, false);
+    console.timeEnd(loading);
+    console.log("Reload it with Ctrl+R or as follows:\nlocation.reload(true)");
+    console.log("injection into " + document.URL + " in\n" + JSON.stringify(navigator.userAgent) + "\nends at\n" + JSON.stringify(Date()));
 })();
--- LastScroll/last_scroll_event_page.js
+++ LastScroll/last_scroll_event_page.js
@@ -1,4 +1,15 @@
 (function() {
+    var loading = "loading started at " + new Error().stack.split(/\s+/)[2] + "\n(" + (chrome.app.getDetails() && chrome.app.getDetails().name || "no chrome.app.getDetails()") + ") takes";
+    console.time(loading);
+    //TODO Place following code where timed section should end.
+    //console.timeEnd(loading);
+    //console.log("Reload it with Ctrl+R or as follows:\nlocation.reload(true)");
+    //console.log("injection into " + document.URL + " in\n" + JSON.stringify(navigator.userAgent) + "\nends at\n" + JSON.stringify(Date()));
+    //window.addEventListener('readystatechange', function(event) {
+    //    if (event.target.readyState !== "complete") {
+    //        return;
+    //    }
+    //}, false);
     // TODO See http://developer.chrome.com/extensions/runtime.html#event-onInstalled
     chrome.runtime.onInstalled.addListener(function(details) {
         try {
@@ -23,8 +34,13 @@
             }
         }
     });
+    console.timeEnd(loading);
+    console.log("Reload it with Ctrl+R or as follows:\nlocation.reload(true)");
+    console.log("injection into " + document.URL + " in\n" + JSON.stringify(navigator.userAgent) + "\nends at\n" + JSON.stringify(Date()));
 })();
 //Sample alert content in M33. The alert title is not part of the content copied by Ctrl+A Ctrl+C
+//Sat Nov 16 2013 18:34:19 GMT+0100 (Westeuropäische Normalzeit)Last Scroll hat ein update von Version 7 auf 8 erhalten
+//Sat Nov 16 2013 16:51:55 GMT+0100 (Westeuropäische Normalzeit)Last Scroll witnessed a chrome_update to version Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1710.0 Safari/537.36
 //Fri Nov 15 2013 01:57:04 GMT+0100 (Westeuropäische Normalzeit)Last Scroll got an update from version 6 to 7
 //Fri Nov 15 2013 01:50:28 GMT+0100 (Westeuropäische Normalzeit)Last Scroll hat ein update von Version 6 auf 7 erhalten
 //Fri Nov 15 2013 01:49:53 GMT+0100 (Westeuropäische Normalzeit)Last Scroll witnessed a chrome_update to version Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.57 Safari/537.36
